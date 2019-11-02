7f86c93cadae0d2cbf4c5da14afe27e7dbc91045
matt319940 <matt319940@yahoo.com>
2019-11-01 22:36:51 -0400

    Touched up a side nav

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,12 +2,10 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "50px";
-    document.body.style.backgroundColor = "white";
 }
